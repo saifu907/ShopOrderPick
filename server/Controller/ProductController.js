142c4e753924cd3c57b38b0feb5082a4a329cbe9
saifu907 <saifunazer54@gmail.com>
2024-10-28 21:26:02 +0530

    first

--- server/Controller/ProductController.js
+++ server/Controller/ProductController.js
@@ -6,7 +6,7 @@ const cloudinary = require('cloudinary').v2;
 cloudinary.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.API_KEY,
-  api_secret: process.env.SEARCHKEY,
+  api_secret: process.env.API_SECRET,
 });
 
 exports.addProducts = async (req, res) => {
